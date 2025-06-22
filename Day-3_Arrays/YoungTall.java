42212d604eb6af5381f22b2026cce29b8d094447
Vaibhavi01 <varshneyvaibhavi65@gmail.com>
2025-06-22 19:30:09 +0530

    [Vaibhavi commits young and tall friend]

---
+++ Day-3_Arrays/YoungTall.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class YoungTall {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] names = {"Amar", "Akbar", "Anthony"};
+        int[] ages = new int[3];
+        double[] heights = new double[3];
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Enter details for " + names[i] + ":");
+            System.out.print("Age: ");
+            ages[i] = sc.nextInt();
+            System.out.print("Height (in cm): ");
+            heights[i] = sc.nextDouble();
+            System.out.println();
+        }
+        int youngestIndex = 0;
+        for (int i = 1; i < 3; i++) {
+            if (ages[i] < ages[youngestIndex]) {
+                youngestIndex = i;
+            }
+        }
+        int tallestIndex = 0;
+        for (int i = 1; i < 3; i++) {
+            if (heights[i] > heights[tallestIndex]) {
+                tallestIndex = i;
+            }
+        }
+        System.out.println("The youngest friend is: " + names[youngestIndex] + " (Age: " + ages[youngestIndex] + ")");
+        System.out.println("The tallest friend is: " + names[tallestIndex] + " (Height: " + heights[tallestIndex] + " cm)");
+        sc.close();
+    }
+}
